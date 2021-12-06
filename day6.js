8ab62484b85f85f5b31dd92fb7fa9d86e098bd75
Rachel Kaufman <16939591+rkaufman13@users.noreply.github.com>
2021-12-06 14:08:54 -0500

    part 2 in non-computer crashy mode

--- day6.js
+++ day6.js
@@ -6,26 +6,56 @@ const input = fs
   .split(",")
   .map((i) => Number(i));
 let array = input;
-const process = (array, day) => {
+
+let counts = {};
+array.forEach(function (x) {
+  counts[x] = (counts[x] || 0) + 1;
+});
+
+const part1process = (array, day) => {
   let initialArrayLength = array.length;
-  //console.log(`beginning of day ${day}: ${array}`);
+
   for (let i = 0; i < initialArrayLength; i++) {
     if (array[i] > 0) {
       array[i] = array[i] - 1;
-      //   if (array[i] == 6) {
-
-      //   }
     } else {
       array[i] = 6;
       array.push(8);
     }
   }
-
   return array;
 };
 
-for (let i = 0; i < 256; i++) {
-  array = process(array, i);
+const part2process = (object) => {
+  object["temp"] = object["0"] || 0;
+  for (let i = 0; i < 8; i++) {
+    if (i < 8) {
+      const nextNum = i + 1;
+      if (object.hasOwnProperty(nextNum)) {
+        if (i == 6) {
+          object[i] = object[nextNum] + object["temp"];
+        } else {
+          object[i] = object[nextNum];
+        }
+      } else {
+        object[i] = 0;
+      }
+    }
+  }
+  object["8"] = object["temp"];
+  return object;
+};
+
+//part1
+for (let i = 0; i < 80; i++) {
+  array = part1process(array, i);
 }
 
+//part2
+for (let i = 0; i < 256; i++) {
+  counts = part2process(counts, i);
+}
+delete counts.temp;
+
 console.log(array.length);
+console.log(Object.values(counts).reduce((a, b) => a + b));
